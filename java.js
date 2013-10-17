40391d4475ac4041afe8025661c714a4957f0b5f
Max Goldman <maxg@mit.edu>
2013-10-17 18:05:31 -0400

    Initial version of Java compilation & execution

---
+++ java.js
@@ -0,0 +1,101 @@
+var events = require('events');
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+var temp = require('temp');
+
+exports.compileAndRun = function(files, package, main, callback) {
+  if (package.length > 0 && package.substr(-1) != '.') {
+    package = package + '.';
+  }
+  var sources = {};
+  files.forEach(function(file) {
+    sources[package + file.filename.replace(/\.java$/, '')] = file.data;
+  });
+  
+  javac(sources).once('done', function(err, compile, zip) {
+    if (err) { return callback(err); }
+    java(zip, package + main.replace(/\.java$/, '')).once('done', function(err, run) {
+      if (err) { return callback(err); }
+      callback(null, { compile: compile, run: run });
+      setTimeout(function() { fs.unlinkSync(zip); }, 60000);
+    });
+  });
+};
+
+function nice() {
+  var child = spawn('nice', Array.prototype.slice.call(arguments));
+  var sigterm = setTimeout(function() { child.emit('timeout'); child.kill(); }, 2000);
+  var sigkill = setTimeout(function() { child.kill('SIGKILL'); }, 3000);
+  child.on('exit', function() {
+    clearTimeout(sigterm);
+    clearTimeout(sigkill);
+  });
+  return child;
+}
+
+function javac(sources) {
+  var emitter = new events.EventEmitter();
+  
+  temp.open({ suffix: '.zip' }, function(err, info) {
+    var zip = info.path;
+    var javac = nice('java', '-cp', 'bin', 'coconut.Compile', JSON.stringify(sources));
+    
+    javac.stdout.pipe(fs.createWriteStream(zip));
+    
+    var out = '';
+    javac.stderr.setEncoding('utf8');
+    javac.stderr.on('data', function(data) { out += data; });
+    
+    javac.once('close', function() {
+      try {
+        emitter.emit('done', null, JSON.parse(out), zip);
+      } catch (err) {
+        console.error('Error compiling Java', out);
+        emitter.emit('done', err);
+      }
+    });
+    
+    fs.close(info.fd, function() { });
+  });
+  return emitter;
+}
+
+function java(zip, main) {
+  var emitter = new events.EventEmitter();
+  
+  var child = nice('java', '-Djava.security.manager', '-cp', zip, main);
+  
+  var out = [];
+  var stdout = function(data) { out.append({ $: data }); };
+  var stderr = function(data) { out.append({ $: data, err: true }); };
+  Object.defineProperty(out, 'append', { value: function(line) {
+    this.push(line);
+    if (this.length > 512) {
+      child.kill();
+      this.push({ truncated: true });
+      child.stdout.removeListener('data', stdout);
+      child.stderr.removeListener('data', stderr);
+    }
+  } });
+  child.stdout.setEncoding('utf8');
+  child.stdout.on('data', stdout);
+  child.stderr.setEncoding('utf8');
+  child.stderr.on('data', stderr);
+  
+  child.once('timeout', function() { out.push({ timeout: true }); });
+  child.once('close', function() { emitter.emit('done', null, out); });
+  
+  return emitter;
+}
+
+if (require.main === module) {
+  var compile = javac(require('./' + process.argv[2]));
+  compile.once('done', function(err, out, zip) {
+    console.log('--compile--\n', out);
+    var run = java(zip, 'Main');
+    run.on('done', function(err, out) {
+      console.log('--run--\n', out);
+      fs.unlinkSync(zip);
+    });
+  });
+}
---
+++ java/coconut/Compile.java
@@ -0,0 +1,160 @@
+package coconut;
+
+import java.io.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.tools.*;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileObject.Kind;
+
+import org.json.JSONObject;
+
+public class Compile {
+    
+    final boolean success;
+    final String output;
+    final List<Diagnostic<? extends JavaFileObject>> errors;
+    final List<JavaClassBytes> classes;
+    
+    Compile(List<JavaSourceString> sources) {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        
+        InMemoryFileManager fileManager = new InMemoryFileManager();
+        
+        StringWriter compilation = new StringWriter();
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+        CompilationTask task = compiler.getTask(compilation, fileManager, diagnostics, null, null, sources);
+        this.success = task.call();
+        this.output = compilation.toString();
+        this.errors = diagnostics.getDiagnostics();
+        this.classes = fileManager.outputs;
+    }
+    
+    private static List<JSONObject> json(List<Diagnostic<? extends JavaFileObject>> diagnostics) {
+        List<JSONObject> objects = new ArrayList<JSONObject>();
+        for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics) {
+            JSONObject obj = new JSONObject();
+            obj.put("lineNumber", diagnostic.getLineNumber());
+            obj.put("columnNumber", diagnostic.getColumnNumber());
+            obj.put("startPosition", diagnostic.getStartPosition());
+            obj.put("position", diagnostic.getPosition());
+            obj.put("endPosition", diagnostic.getEndPosition());
+            obj.put("kind", diagnostic.getKind());
+            obj.put("message", diagnostic.getMessage(null));
+            obj.put("source", diagnostic.getSource().getName());
+            objects.add(obj);
+        }
+        return objects;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        JSONObject in = new JSONObject(args[0]);
+        List<JavaSourceString> sources = new ArrayList<JavaSourceString>();
+        for (Object key : in.keySet()) {
+            String name = (String)key;
+            sources.add(new JavaSourceString(name, in.getString(name)));
+        }
+        
+        Compile compile = new Compile(sources);
+        
+        if (compile.success && ! compile.classes.isEmpty()) {
+            ZipOutputStream zip = new ZipOutputStream(System.out);
+            for (JavaClassBytes bytes : compile.classes) {
+                zip.putNextEntry(new ZipEntry(bytes.getName()));
+                zip.write(bytes.bytes());
+                zip.closeEntry();
+            }
+            zip.close();
+        }
+        JSONObject err = new JSONObject();
+        err.put("output", compile.output);
+        err.put("errors", json(compile.errors));
+        System.err.println(err);
+    }
+}
+
+class InMemoryFileManager extends ForwardingJavaFileManager<JavaFileManager> {
+    
+    public final List<JavaClassBytes> outputs = new ArrayList<JavaClassBytes>();
+    
+    InMemoryFileManager() {
+        super(ToolProvider.getSystemJavaCompiler().getStandardFileManager(null, null, null));
+    }
+    
+    @Override public FileObject getFileForInput(Location location, String packageName, String relativeName) throws IOException {
+        throw new IOException("Unsupported getFileForInput");
+    }
+    
+    @Override public FileObject getFileForOutput(Location location, String packageName, String relativeName, FileObject sibling) throws IOException {
+        throw new IOException("Unsupported getFileForOutput");
+    }
+    
+    @Override public JavaFileObject getJavaFileForInput(Location location, String className, Kind kind) throws IOException {
+        throw new IOException("Unsupported getJavaFileForInput");
+    }
+    
+    @Override public JavaFileObject getJavaFileForOutput(Location location, String className, Kind kind, FileObject sibling) throws IOException {
+        if (location != StandardLocation.CLASS_OUTPUT) { throw new IOException("Output location must be CLASS_OUTPUT"); }
+        if (kind != Kind.CLASS) { throw new IOException("Output kind must be CLASS"); }
+        
+        JavaClassBytes file = new JavaClassBytes(className);
+        outputs.add(file);
+        return file;
+    }
+}
+
+class JavaSourceString extends SimpleJavaFileObject {
+    
+    private final String fileName;
+    private final String code;
+    
+    JavaSourceString(String className, String code) {
+        this(className, className.replace('.', '/') + Kind.SOURCE.extension, code);
+    }
+    
+    private JavaSourceString(String className, String fileName, String code) {
+        super(URI.create("string:///" + fileName), Kind.SOURCE);
+        this.fileName = fileName;
+        this.code = code;
+    }
+    
+    @Override public String getName() {
+        return fileName;
+    }
+    
+    @Override public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+        return code;
+    }
+}
+
+class JavaClassBytes extends SimpleJavaFileObject {
+    
+    private final String fileName;
+    private final ByteArrayOutputStream bytes;
+    
+    JavaClassBytes(String className) {
+        this(className, className.replace('.', '/') + Kind.CLASS.extension);
+    }
+    
+    private JavaClassBytes(String className, String fileName) {
+        super(URI.create("bytes:///" + fileName), Kind.CLASS);
+        this.fileName = fileName;
+        this.bytes = new ByteArrayOutputStream();
+    }
+    
+    byte[] bytes() {
+        return bytes.toByteArray();
+    }
+    
+    @Override public String getName() {
+        return fileName;
+    }
+    
+    @Override public OutputStream openOutputStream() {
+        return bytes;
+    }
+}
